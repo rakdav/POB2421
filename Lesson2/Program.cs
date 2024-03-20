1e9f704b70497acd52833711db6c66eb5ecb1398
rakdav <rakdav@inbox.ru>
2024-03-20 21:22:07 +0200

    lesson2last

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -4,7 +4,22 @@
 //Console.WriteLine((double)5/9);
 //int x = int.Parse(Console.ReadLine());
 //int y=Convert.ToInt32(Console.ReadLine());
-string myName = "Иван\nИванов";
-Console.WriteLine(myName);
-string myString = "284"; 
-Console.WriteLine(int.Parse(myString));
+//string myName = "Иван\nИванов";
+//Console.WriteLine(myName);
+//string myString = "284"; 
+//Console.WriteLine(int.Parse(myString));
+//using System.Diagnostics;
+
+//byte bt=byte.Parse(Console.ReadLine());
+//long freq = Stopwatch.Frequency;
+//Stopwatch stopwatch = new Stopwatch();
+//stopwatch.Start();
+////int integer = (int)bt;
+//double l = (double)bt;
+//stopwatch.Stop();
+//decimal sec=(decimal)stopwatch.ElapsedTicks/freq;
+//Console.WriteLine($"bt = {bt} \r\n Частота таймера"+ $"{freq} такт/с \r\n Время в тактах {stopwatch.ElapsedTicks} \r\n Время в секундах "+ $"{sec}");
+//Console.WriteLine(l.ToString());
+//Console.WriteLine($"{Math.PI:F2}");
+//Console.WriteLine($"{Math.E:F1}");
+Console.WriteLine(Math.Sin(60*Math.PI/180));
