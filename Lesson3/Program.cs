c61af9ce0f7b7d8e5b715f5252e475e6f4e790b8
rakdav <rakdav@inbox.ru>
2024-03-25 19:43:28 +0200

    lesson3-2

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -35,13 +35,17 @@ Console.Write("Введите количество:");
 int count3 = int.Parse(Console.ReadLine());
 Console.Write("Введите цену:");
 decimal price3 = int.Parse(Console.ReadLine());
-
+decimal sum=count1*price1+count2*price2 + count3*price3;
 Console.Title = "Чек";
-Console.WriteLine("--------------------------------\n" +
-    "*****************************");
-
-Console.WriteLine($"{"N",-4}{"Название",-10}{"Цена",-6}{"Кол.",-6}{"Сумма",-6}");
-Console.ForegroundColor = ConsoleColor.White;
-Console.WriteLine($"{1,-4}{"",-10}{"Цена",-6}{"Кол.",-6}{"Сумма",-6}\n");
+Console.WriteLine(
+    $"--------------------------------\n" +
+    $"Организация: {company}\n" +
+    $"ИНН:{INN}\n" +
+    $"*********************************\n" +
+    $"{"N",-4}{"Название",-10}{"Цена",-6}{"Кол.",-6}{"Сумма",-6}\n" +
+    $"{1,-4}{name1,-10}{price1,-6}{count1,-6}{price1 * count1,-6}\n" +
+    $"{2,-4}{name2,-10}{price2,-6}{count1,-6}{price2 * count2,-6}\n" +
+    $"{3,-4}{name3,-10}{price3,-6}{count3,-6}{price3 * count3,-6}\n" +
+    $"{"Итог:",-26}{sum}");
 
 
