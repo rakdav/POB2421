6f288c320a6a8dc57b5201d35481061f09998bb4
rakdav <rakdav@inbox.ru>
2024-04-01 19:58:50 +0200

    Lesson5-1

---
+++ Lesson5/Program.cs
@@ -0,0 +1,24 @@
+﻿//Console.WriteLine(9 & 6);
+//Console.WriteLine(16 & 7);
+//Console.WriteLine(8 & 14);
+//Console.WriteLine(5&22);
+
+//Console.WriteLine(5 | 9);
+//Console.WriteLine(8 | 19);
+//Console.WriteLine(3 | 7);
+//Console.WriteLine(22|46);
+
+//Console.WriteLine(9^6);
+//Console.WriteLine(15^6);
+//Console.Write("Введите целое число:");
+//int n=int.Parse(Console.ReadLine());
+//Console.Write("Введите ключ:");
+//int key=int.Parse(Console.ReadLine());
+//int crypt = n ^ key;
+//Console.WriteLine($"Зашифрованное сообщение:{crypt}");
+//int encrypt=crypt^key;
+//Console.WriteLine($"Расшифрованное сообщение:{encrypt}");
+
+Console.WriteLine(~8);
+Console.WriteLine(~256854);
+Console.WriteLine(~2146569854);
