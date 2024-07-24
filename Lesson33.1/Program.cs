b7e737f2de25186851ac76647281cbfaebe4490a
rakdav <rakdav@inbox.ru>
2024-07-24 20:09:16 +0200

    Lesson33.1

---
+++ Lesson33.1/Program.cs
@@ -0,0 +1,24 @@
+﻿string path = "C:\\Users\\C4PCT\\source";
+void PrintDirectory(DirectoryInfo directory, string indent,bool lastDirectory)
+{
+    Console.Write(indent);
+    if(lastDirectory)
+    {
+        Console.Write("∟");
+        indent += " ";
+    }
+    else
+    {
+        Console.Write("├");
+        indent += "| ";
+    }
+    Console.WriteLine(directory.Name);
+    DirectoryInfo[] subDirectories= directory.GetDirectories();
+    for(int i = 0; i < subDirectories.Length; i++)
+    {
+        PrintDirectory(subDirectories[i], indent, i==subDirectories.Length-1);
+    }
+}
+
+PrintDirectory(new DirectoryInfo(path), "", false);
+
