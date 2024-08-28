d915d6de891d92514705e8701aa5b97259d53b72
rakdav <rakdav@inbox.ru>
2024-08-28 21:02:54 +0200

    Last

--- Lesson35/Program.cs
+++ Lesson35/Program.cs
@@ -1,12 +1,10 @@
 ﻿const int WINDOW_WIDTH = 120;
-const int WINDOW_HEIGHT = 40;
+const int WINDOW_HEIGHT = 44;
 DirectoryInfo directoryInfo=new DirectoryInfo(Environment.CurrentDirectory);
-string currentPath = directoryInfo.Root.FullName;
+string currentPath = directoryInfo.FullName;
 Console.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 Console.SetBufferSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-DrawConsole(0, 0, WINDOW_WIDTH, 25);
-DrawConsole(0, 25, WINDOW_WIDTH, 25);
 
 void DrawConsole(int left,int top, int width, int height)
 {
@@ -18,44 +16,73 @@ void DrawConsole(int left,int top, int width, int height)
     {
         Console.Write("│");
         for (int j = 0; j < width - 2; j++) Console.Write(" ");
-        Console.Write("│");
+        Console.WriteLine("│");
     }
     Console.Write("└");
     for (int j = 0; j < width - 2; j++) Console.Write("─");
     Console.WriteLine("┘");
 }
-//do
-//{
-//    Console.Write(currentPath+">");
-//    string command=Console.ReadLine()!;
-//    string[] commands=command.Split(' ');
-//    switch(commands[0])
-//    {
-//        case "cd":
-//            {
-//                switch(commands[1])
-//                {
-//                    case "..":
-//                        {
-//                            directoryInfo = directoryInfo.Parent!;
-//                            currentPath = directoryInfo.FullName;
-//                        }
-//                        break;
-//                    case "/":
-//                        {
-//                            directoryInfo = directoryInfo.Root!;
-//                            currentPath = directoryInfo.FullName;
-//                        }
-//                        break;
-//                    case "~":
-//                        {
-//                            //directoryInfo = ";
-//                            //currentPath = directoryInfo.FullName;
-//                        }
-//                        break;
-//                }
-//            }
-//            break;
-//    }
-//}
-//while (true);
+
+void PrintDirectory(DirectoryInfo directory, string indent, bool lastDirectory)
+{
+    Console.Write(indent);
+    if (lastDirectory)
+    {
+        Console.Write("∟");
+        indent += " ";
+    }
+    else
+    {
+        Console.Write("├");
+        indent += "| ";
+    }
+    Console.WriteLine(directory.Name);
+    DirectoryInfo[] subDirectories = directory.GetDirectories();
+    for (int i = 0; i < subDirectories.Length; i++)
+    {
+        PrintDirectory(subDirectories[i], indent, i == subDirectories.Length - 1);
+    }
+}
+do
+{
+    DrawConsole(0, 0, WINDOW_WIDTH, 25);
+    DrawConsole(0, 25, WINDOW_WIDTH, 8);
+    DrawConsole(0, 33, WINDOW_WIDTH, 4);
+    Console.SetCursorPosition(1, 1);
+    PrintDirectory(directoryInfo, "", false);
+    Console.SetCursorPosition(1, 34);
+    Console.Write(currentPath + ">");
+    string command = Console.ReadLine()!;
+    string[] commands = command.Split(' ');
+    switch (commands[0])
+    {
+        case "cd":
+            {
+                switch (commands[1])
+                {
+                    case "..":
+                        {
+                            directoryInfo = directoryInfo.Parent!;
+                            currentPath = directoryInfo.FullName;
+                        }
+                        break;
+                    case "/":
+                        {
+                            directoryInfo = directoryInfo.Root!;
+                            currentPath = directoryInfo.FullName;
+                        }
+                        break;
+                    case "~":
+                        {
+                            string userName= Environment.UserName;
+                            directoryInfo =directoryInfo.Root;
+                            currentPath = directoryInfo.FullName+"\\Users\\"+userName;
+                            directoryInfo=new DirectoryInfo(currentPath);
+                        }
+                        break;
+                }
+            }
+            break;
+    }
+}
+while (true);
